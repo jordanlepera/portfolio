2bf7cfc017e522f967059bad6dc657422b997519
Jordan Le Pera <leperajordan@gmail.com>
2020-07-25 03:45:59 +0200

    update test of App component

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders nav link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Projec?ts/gi);
   expect(linkElement).toBeInTheDocument();
