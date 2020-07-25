37de184024b40fb8d48b0ee1ef16106df9831c6d
Jordan Le Pera <leperajordan@gmail.com>
2020-07-25 03:52:15 +0200

    Add Project + Menu component

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 const Project = props => {
   const { test } = props;
+  const { t } = useTranslation();
+
   return (
     <div>
+      {t('hello-world')}
       {test}
     </div>
   );
