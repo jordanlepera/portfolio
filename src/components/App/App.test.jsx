70ae09bb59b5abddabdbbc13d1848f2c7ee771fa
Jordan Le Pera <leperajordan@gmail.com>
2020-07-25 23:44:21 +0200

    update dependencies + remove useless comments

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -4,6 +4,5 @@ import App from "./App"
 
 it("test render App component", () => {
   const mounted = mount(<App />)
-  // console.log(mounted.debug());
   expect(mounted.contains(/Projec?ts/gi)).toBe(true)
 })
