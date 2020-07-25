f93e5d53646203f5069eded20ccdf76699d876f0
Jordan Le Pera <leperajordan@gmail.com>
2020-07-25 03:50:15 +0200

    Add responsivness to Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,47 +8,11 @@ import styled from 'styled-components';
 const Footer = () => {
   const matches = useMediaQuery('(max-width:959px)');
 
-  const FooterContainer = styled.footer`
-    border-top: 1px solid #dddddd;
-    display: flex;
-    flex-direction: ${props => props.matches ? 'column' : 'row'};
-    align-items: center;
-    min-height: 80px;
-    max-width: 1000px;
-    margin-top: 30px;
-    width: 90%;
-  `;
-
-  const CopyrightContainer = styled.div`
-    flex-grow: 1;
-    text-align: ${props => props.matches ? 'center' : 'left'};
-    color: #3D3D3D;
-    margin: 10px;
-    padding: 10px;
-  `;
-
-  const SocialLink = styled.a`
-    display: flex;
-    align-items: flex-end;
-    flex-direction: row;
-    text-decoration: none;
-    cursor: pointer;
-    border: none;
-    padding: 10px;
-    margin: 10px;
-    background-color: transparent;
-    color: #3D3D3D;
-    transition: color 0.2s ease-in-out;
-    &:hover {
-      color: rgba(61, 61, 61, 0.5)
-    }
-  `;
-
   return (
     <FooterContainer matches={matches}>
       <Grid container direction={matches ? 'column-reverse' : 'row'} alignItems="center" justify="space-between" style={{ flexGrow: 1 }}>
         <Grid item xs>
-          <CopyrightContainer>
+          <CopyrightContainer matches={matches}>
             2020 © Jordan LE PERA - All rights reserved
           </CopyrightContainer>
         </Grid>
@@ -76,5 +40,41 @@ const Footer = () => {
     </FooterContainer>
   );
 };
+
+const FooterContainer = styled.footer`
+border-top: 1px solid #dddddd;
+display: flex;
+flex-direction: ${props => props.matches ? 'column' : 'row'};
+align-items: center;
+min-height: 80px;
+max-width: 1000px;
+margin-top: 30px;
+width: 90%;
+`;
+
+const CopyrightContainer = styled.div`
+flex-grow: 1;
+text-align: ${props => props.matches ? 'center' : 'left'};
+color: #3D3D3D;
+margin: 10px;
+padding: 10px;
+`;
+
+const SocialLink = styled.a`
+display: flex;
+align-items: flex-end;
+flex-direction: row;
+text-decoration: none;
+cursor: pointer;
+border: none;
+padding: 10px;
+margin: 10px;
+background-color: transparent;
+color: #3D3D3D;
+transition: color 0.2s ease-in-out;
+&:hover {
+  color: rgba(61, 61, 61, 0.5)
+}
+`;
 
 export default Footer;
