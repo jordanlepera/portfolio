bcf1025c03bdca16c5e1c83e53c62716891a92e4
Jordan Le Pera <leperajordan@gmail.com>
2020-08-01 01:15:28 +0200

    Add Articles page breadcrumbs

--- src/pages/Articles/Articles.jsx
+++ src/pages/Articles/Articles.jsx
@@ -1,20 +1,37 @@
 // @flow
 import React from "react"
 import styled from "styled-components"
-// import { useTranslation } from 'react-i18next';
+import Typography from "@material-ui/core/Typography"
+import Breadcrumbs from "@material-ui/core/Breadcrumbs"
+import Link from "@material-ui/core/Link"
+import Fade from "@material-ui/core/Fade"
+import { useTranslation } from "react-i18next"
 import WorkInProgress from "../../components/WorkInProgress/WorkInProgress"
 
 const Articles = () => {
-  // const { t } = useTranslation();
+  const { t } = useTranslation()
 
   return (
-    <>
+    <Fade in timeout={500}>
       <Container>
+        <Typography variant="h3">{t("articles")}</Typography>
+        <Breadcrumbs aria-label="breadcrumb">
+          <Link color="inherit" href="/">
+            {t("home")}
+          </Link>
+          <Typography color="textPrimary">{t("articles")}</Typography>
+        </Breadcrumbs>
+        <Space />
         <WorkInProgress />
       </Container>
-    </>
+    </Fade>
   )
 }
+
+const Space = styled.div`
+  width: 100%;
+  height: 10px;
+`
 
 const Container = styled.div`
   width: 90%;
